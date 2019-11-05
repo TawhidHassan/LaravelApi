b0778cb931eaf90157d739ae936d55252a5d8c41
sifat hassan <sifat27.sh22@gmail.com>
2019-11-05 19:23:45 +0600

    modefi the api result Specified product by ProductResource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->detail,
             'price'=>$this->price,
-            'stock'=>$this->stock,
+            'stock'=>$this->stock==0 ? 'out of stock': $this->stock,
             'discount'=>$this->discount,
+            'totalPrice'=>round($this->price - $this->discount,2),
+            'rating'=>$this->reviews->count()>0? round($this->reviews->sum('star')/$this->reviews->count()):'No rating yeet',//round use to remove 0.0 into 0
+            'href'=>[
+                'reviews'=>route('reviews.index',$this->id)
+            ]
         ];
     }
 }
