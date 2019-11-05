f9e5090a624740a36eb2f86052b11234beac78b3
sifat hassan <sifat27.sh22@gmail.com>
2019-11-05 19:31:36 +0600

    modefi the result all products by ProductCollection

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Resources\Product;
 
+use Illuminate\Http\Resources\Json\Resource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +15,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'name'=>$this->name,
+            'price'=>$this->price,
+            'discount'=>$this->discount,
+            'rating'=>$this->reviews->count()>0? round($this->reviews->sum('star')/$this->reviews->count()):'No rating yeet',//round use to remove 0.0 into 0
+            'href'=>[
+                'link'=>route('products.show',$this->id)
+            ]
+        ];
     }
 }
